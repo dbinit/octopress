fe8949d41ffd71ced27ddece4b54b6b664e4999d
David Bennett <david@dbinit.com>
2012-01-25 20:33:40 -0600

    Tweak quotes for tag cloud html tags.

--- plugins/tag_cloud.rb
+++ plugins/tag_cloud.rb
@@ -7,19 +7,19 @@ module Jekyll
       begin
         tags = context['site']['tags'].map{|tag|
           {
-            "title"    => tag[0],
-            "posts"    => tag[1]
+            'title'    => tag[0],
+            'posts'    => tag[1]
           }
         }
         unless tags.nil?
-          min_count = tags.min{|a, b| a["posts"].length <=> b["posts"].length }["posts"].length
-          max_count = tags.max{|a, b| a["posts"].length <=> b["posts"].length }["posts"].length
+          min_count = tags.min{|a, b| a['posts'].length <=> b['posts'].length }['posts'].length
+          max_count = tags.max{|a, b| a['posts'].length <=> b['posts'].length }['posts'].length
 
-          weights = tags.inject({}){|result, tag| result[tag["title"]] = ( ((tag["posts"].length - min_count) * (280 - 75)) / (max_count - min_count) ) + 75; result }
+          weights = tags.inject({}){|result, tag| result[tag['title']] = ( ((tag['posts'].length - min_count) * (280 - 75)) / (max_count - min_count) ) + 75; result }
 
-          tags.inject("") { |html, tag|
-            s << "<span style='font-size: #{sprintf("%d", weights[tag['title']])}%'>"
-            s << "<a href='/tag/#{tag['title'].gsub(/_|\W/, '-')}/'>#{tag["title"]}</a>"
+          tags.inject('') { |html, tag|
+            s << "<span style=\"font-size: #{sprintf("%d", weights[tag['title']])}%\">"
+            s << "<a href=\"/tag/#{tag['title'].gsub(/_|\W/, '-')}/\">#{tag["title"]}</a>"
             s << "</span>\n"
           }
         end
